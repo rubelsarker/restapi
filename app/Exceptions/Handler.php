1fd49c16a2b16db292f0ba7ed308a48395f7372f
unknown <you@example.com>
2019-03-03 14:36:28 +0600

    Handle Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,11 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,26 +30,30 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
+
     public function report(Exception $exception)
     {
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
+
     public function render($request, Exception $exception)
     {
+
+        if($request->expectsJson()){
+            if($exception instanceof ModelNotFoundException){
+                return response()->json([
+                    'errors'=>'Product Model Not Found'
+                ],Response::HTTP_NOT_FOUND);
+            }
+        }
+        if($exception instanceof NotFoundHttpException){
+            return response()->json([
+                'errors'=>'Incorrect Route'
+            ],Response::HTTP_NOT_FOUND);
+        }
+
+
         return parent::render($request, $exception);
     }
 }
